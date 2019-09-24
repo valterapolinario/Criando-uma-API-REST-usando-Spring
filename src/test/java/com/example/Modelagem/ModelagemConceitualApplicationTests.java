73ca4215c4a690c71649499cae8a3965340ae50b
valterapolinario <valter_123@hotmail.com>
2019-09-24 09:00:51 -0300

    projeto criado

---
+++ src/test/java/com/example/Modelagem/ModelagemConceitualApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.Modelagem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ModelagemConceitualApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
